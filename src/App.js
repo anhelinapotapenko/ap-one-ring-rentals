fac6cd11d23cc2a3a6447d0ff37b28f09cf4398a
anhelinapotapenko <potapenkoanhelina@gmail.com>
2026-06-01 15:14:29 +0800

    Add Routes, updated App.js

--- src/App.js
+++ src/App.js
@@ -2,15 +2,22 @@ import React from "react";
 
 import "./css/bootstrap.min.css";
 import "./css/style.css";
+
 import HomePage from "./pages/HomePage.js";
+import ContactPage from "./pages/ContactPage.js";
+
 import Header from "./components/Header.js";
 import Footer from "./components/Footer.js";
+import { Routes, Route } from "react-router-dom";
 
 function App() {
   return (
     <>
       <Header />
-      <HomePage />
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/contact" element={<ContactPage />} />
+      </Routes>
       <Footer />
     </>
   );
